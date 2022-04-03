ae28bc56273cf02bb2743734fab383267c633306
masteray <masteraychen@gmail.com>
2022-04-03 22:33:51 +0800

    debug getActions

--- frontend/src/pages/account/index.vue
+++ frontend/src/pages/account/index.vue
@@ -233,7 +233,7 @@ export default defineComponent({
 
         // Token transfer      Actions (Raw Data)        Actions info
         const getActions = (account: string, pos: number) => {
-            // account = 'amax';
+            account = 'amax';
             pos = pos === 1 ? -1 : pos;
             GET_ACTIONS(account, pos, 100).then((res: any) => {
                 console.log('getActions-----', res);
