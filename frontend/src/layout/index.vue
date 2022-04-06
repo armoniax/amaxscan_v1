11a62e0857061278bdc4468e3c733f39be93bca6
imlun <1078966013@qq.com>
2022-04-06 14:57:51 +0800

    LiveActions

--- frontend/src/layout/index.vue
+++ frontend/src/layout/index.vue
@@ -2,25 +2,38 @@
 .flex.flex-col.text-sm.font-semibold.h-screen
   LayoutHeader
   .flex-1(class="overflow-auto")
-    LayoutLive
+    LayoutLive(v-model.show="show")
+    LayoutActions(v-show="show")
     router-view.container
   LayoutFooter
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, watch } from 'vue'
 import LayoutHeader from './components/Header.vue'
 import LayoutFooter from './components/Footer.vue'
 import LayoutLive from './components/LiveActions.vue'
+import LayoutActions from './components/Actions.vue'
+import { useRoute } from 'vue-router'
 
 export default defineComponent({
   components: {
     LayoutHeader,
     LayoutFooter,
-    LayoutLive
+    LayoutLive,
+    LayoutActions
   },
   setup() {
-    
+    const route = useRoute()
+    const show = ref(route.path === '/')
+
+    watch(()=>route.path, r=>{
+      show.value = r === '/'
+    })
+
+    return {
+      show
+    }
   },
 })
 </script>
