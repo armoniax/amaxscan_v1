33ea71e73e9af8fce57559d4e71ef1a5e901e315
april <hesiyuetian@gmail.com>
2022-04-12 14:26:56 +0800

    fixed: bug

--- frontend/src/layout/components/Header.vue
+++ frontend/src/layout/components/Header.vue
@@ -1,17 +1,17 @@
 <template lang="pug">
-    .lg_p-4.p-2
+.lg_p-4.p-2
     Wrapper.flex.items-center
-    router-link.mr-2(to='/')
-    img.object-contain.lg_w-24.w-16(src='@/assets/images/logo.png', style='image-rendering: -webkit-optimize-contrast')
-    .flex-1.flex.space-x-8.items-center.justify-end.text-sm.font-semibold
-    .w-full.flex.lg_w-80.h-8.bg-gray-eee.rounded-full.px-2.items-center
-    input.pl-4.bg-transparent.w-full.h-full.outline-none.text-xs(placeholder='Search by Address / Txn Hash / Account / Block', v-model='keyword', @keyup='changeInput')
-    i.text-xl.fal.fa-search.text-gray-ca.mr-1.cursor-pointer(@click='search()')
-    //- span.btn.btn-xs.rounded-full(:class='keyword ? "cursor-pointer" : "opacity-50 cursor-not-allowed"', @click='search') Search
-    .space-x-4.hidden.lg_block
-        router-link(to='/analytics') ANALYTICS
-        router-link(to='/ram') RAM
-        router-link(to='/producers') PRODUCERS
+        router-link.mr-2(to='/')
+            img.object-contain.lg_w-24.w-16(src='@/assets/images/logo.png', style='image-rendering: -webkit-optimize-contrast')
+        .flex-1.flex.space-x-8.items-center.justify-end.text-sm.font-semibold
+            .w-full.flex.lg_w-80.h-8.bg-gray-eee.rounded-full.px-2.items-center
+                input.pl-4.bg-transparent.w-full.h-full.outline-none.text-xs(placeholder='Search by Address / Txn Hash / Account / Block', v-model='keyword', @keyup='changeInput')
+                i.text-xl.fal.fa-search.text-gray-ca.mr-1.cursor-pointer(@click='search()')
+                //- span.btn.btn-xs.rounded-full(:class='keyword ? "cursor-pointer" : "opacity-50 cursor-not-allowed"', @click='search') Search
+            .space-x-4.hidden.lg_block
+                router-link(to='/analytics') ANALYTICS
+                router-link(to='/ram') RAM
+                router-link(to='/producers') PRODUCERS
     .lg_hidden.flex.pt-4.space-x-4.flex-nowrap.overflow-x-scroll
         router-link(to='/analytics') ANALYTICS
         router-link(to='/ram') RAM
