c631d06c22a9a1504136f075dddd5197f19ce816
masteray <masteraychen@gmail.com>
2022-04-30 13:43:57 +0800

    fix sitemap url

--- backend/server/daemons/sitemap.create.js
+++ backend/server/daemons/sitemap.create.js
@@ -7,7 +7,7 @@ fs.createReadStream(path.join(__dirname, '../../accs.csv'))
   .pipe(csv())
   .on('data', function (data) {
       fs.appendFile(path.join(__dirname, '../../sitemap.xml'), 
-      	`<url><loc>https://eosweb.net/account/${data.account_name}</loc></url>
+      	`<url><loc>https://amaxscan.io/account/${data.account_name}</loc></url>
       	`, (err) => {
   		 if (err) throw err;
  		 console.log(count++);
