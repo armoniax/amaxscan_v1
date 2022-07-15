3462cd650002690aba0e01f6c55c059311efe23e
Joss <281900360@qq.com>
2022-07-15 15:49:53 +0800

    update config

--- backend/config.prod.k8s.js
+++ backend/config.prod.k8s.js
@@ -43,7 +43,7 @@ config.telegram = {
 
 // reserved nodes
 config.endpoints = [
-  'http://172.31.12.214:8889',  //localhost access
+  'http://sg-exp3.vmi.nchain.me:8889',  //localhost access
 	'https://expnode.amaxscan.io'
 ];
 
@@ -62,10 +62,10 @@ config.eosConfig = {
 };
 
 // api url for producers list
-config.customChain = process.env.CUSTOMCHAIN || 'http://172.31.12.214:8889';
+config.customChain = process.env.CUSTOMCHAIN || 'http://sg-exp3.vmi.nchain.me:8889';
 
 // api url for history
-config.historyChain = process.env.HISTORYCHAIN || 'http://172.31.12.214:8889'; //still the RPC chain
+config.historyChain = process.env.HISTORYCHAIN || 'http://sg-exp3.vmi.nchain.me:8889'; //still the RPC chain
 
 // tokens api
 //config.tokensAPI = 'http://api.light.xeos.me/api/account/eos/';
